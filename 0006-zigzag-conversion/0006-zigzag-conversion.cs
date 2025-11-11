0fc53f8c0b309376a0f26f61c4132c5a0ea7db81
Jacob Fernandez <90419475+JakeeUp@users.noreply.github.com>
2025-11-11 17:53:24 -0600

    Time: 11 ms (49.44%), Space: 53.6 MB (41.09%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public string Convert(string s, int numRows) {
+        if (numRows == 1) return s;
+        
+        List<StringBuilder> rows = new List<StringBuilder>();
+        for (int i = 0; i < numRows; i++) {
+            rows.Add(new StringBuilder());
+        }
+        
+        int currentRow = 0;
+        int direction = -1; 
+        
+        foreach (char c in s) {
+            rows[currentRow].Append(c);
+            
+            if (currentRow == 0 || currentRow == numRows - 1) {
+                direction *= -1;
+            }
+            
+            currentRow += direction;
+        }
+        
+        StringBuilder result = new StringBuilder();
+        foreach (var row in rows) {
+            result.Append(row);
+        }
+        
+        return result.ToString();
+    }
+}
