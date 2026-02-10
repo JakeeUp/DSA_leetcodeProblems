a544275a008ab27a6f0c0e9a2eff7719909ef7e8
Jacob Fernandez <90419475+JakeeUp@users.noreply.github.com>
2026-02-10 00:20:40 -0600

    Time: 1914 ms (53.67%), Space: 530.5 MB (27.88%) - LeetHub

---
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {  
+        int n = nums.size();
+        int maxLen = 0;
+
+        for (int i = 0; i < n; ++i) {
+            unordered_set<int> evens;
+            unordered_set<int> odds;
+
+            for (int j = i; j < n; ++j) {
+                if (nums[j] % 2 == 0) {
+                    evens.insert(nums[j]);
+                } else {
+                    odds.insert(nums[j]);
+                }
+
+                if (evens.size() == odds.size()) {
+                    maxLen = max(maxLen, j - i + 1);
+                }
+            }
+        }
+        return maxLen;
+    }
+};
