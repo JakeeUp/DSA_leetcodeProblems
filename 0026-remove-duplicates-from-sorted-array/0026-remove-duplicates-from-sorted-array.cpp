3f24ecc607508db282f476af066e33acb006fcbd
Jacob Fernandez <90419475+JakeeUp@users.noreply.github.com>
2026-09-23 03:09:28 -0500

    Time: 12 ms (5.37%), Space: 22.7 MB (51.94%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,19 +1,22 @@
+
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+
+        //exit function if theres nothing in the array
         if(nums.empty())
             return 0;
 
+        //we need a pointer to start off where we looking at in the array
         int i = 0;
 
+        //so now we need something that will keep moving forward in the array to check
         for(int j = 1; j < nums.size(); j++)
         {
-            
             if(nums[j] != nums[i])
             {
                 i++;
                 nums[i] = nums[j];
-
             }
 
         }
@@ -22,6 +25,8 @@ public:
 
         
 
+
+    
 
     }
 };
